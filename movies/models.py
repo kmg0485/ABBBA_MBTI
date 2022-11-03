102bcfa01eb12fa546311588c36eb96300648a51
kmg0485 <kmg0485@naver.com>
2022-11-03 22:08:09 +0900

    :sparkles: Feat : machine learning #20

--- movies/models.py
+++ movies/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-# from users.models import User
+from users.models import User
 
 class Movie(models.Model) :
     movie_id = models.IntegerField(primary_key = True, unique=True)
     title = models.CharField(max_length=100)
     poster = models.TextField()
     description = models.TextField(null=True)
-    # likes = models.ManyToManyField(User, related_name="movie_like")
+    likes = models.ManyToManyField(User, related_name="movie_like")
     
     def __str__(self) :
         return str(self.title)
+    
+    
