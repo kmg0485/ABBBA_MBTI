3b86d6804244e5e8f5ada15df16f8a04015a13f6
kmg0485 <kmg0485@naver.com>
2022-11-04 20:54:34 +0900

    :art: Update : machine learning

--- movies/loaders_movie.py
+++ movies/loaders_movie.py
@@ -1,14 +1,15 @@
-"""
+
 import json
 import csv
 
 # Opening JSON file and loading the data
 # into the variable data
+#경로설정이 상대경로로 하면 인식이 안되어서 부득이하게 절대경로로 설정.
 with open(r'/Users/kimminkyu/Desktop/naebaecamp/6_DRF/ABBBA_MBTI/ABBBA_MBTI/movies/movie_movie.json', 'r', encoding = 'utf-8') as json_file:
     jsondata = json.load(json_file)
  
 # now we will open a file for writing
-data_file = open('movie_movie.csv', 'w', newline='')
+data_file = open('movies/movie_movie.csv', 'w', newline='')
 
 # create the csv writer object
 csv_writer = csv.writer(data_file)
@@ -28,4 +29,3 @@ for data in jsondata:
     csv_writer.writerow(data["fields"].values())
  
 data_file.close()
-"""
