4666bbc0b9ac1fd5257490c9b0a484338b4cdcd6
MINKYUOH <dhalsrbb123@gmail.com>
2022-11-04 16:23:53 +0900

    :art: implement : Articles Likes #33
    Add Articles Likes.

--- articles/serializers.py
+++ articles/serializers.py
@@ -13,7 +13,7 @@ class CommentCreateSerializer(serializers.ModelSerializer):
 class ArticleSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many= True)
     likes = serializers.StringRelatedField(many=True)
-     
+      
     class Meta:
         model = Article
         fields = "__all__"
